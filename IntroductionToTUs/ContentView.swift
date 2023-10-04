5ba0415e0c05cf6f1e194241dd7cfbab18abe099
João Guilherme <joaogacanuto@gmail.com>
2023-10-04 18:55:06 -0300

    init

---
+++ IntroductionToTUs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntroductionToTUs
+//
+//  Created by Joao Guilherme Araujo Canuto on 04/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntroductionToTUs/IntroductionToTUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionToTUsApp.swift
+//  IntroductionToTUs
+//
+//  Created by Joao Guilherme Araujo Canuto on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionToTUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
